38e809ee03a0f3865dedf25417f2be158579d031
CM-Gimenez <75501546+CM-Gimenez@users.noreply.github.com>
2023-01-25 22:35:29 -0300

    cambio de imagen jpg

--- js/index.js
+++ js/index.js
@@ -19,7 +19,7 @@ let meData = [
     localidad: "Santa Elena",
     provincia: "Entre Rios",
     estadoCivil: "Soltero",
-    img: "../img/perfil.jpg",
+    img: "../img/perfil.png",
     secundario: "Bachiller Informático Contable",
     institucionSec: "Instituto Santa Rosa de Lima D-103",
     titulo: "Técnico Universitario en Tecnologías de la Información",
